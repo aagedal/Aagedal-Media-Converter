b6b983366e25060e8fa36cc3ecee73521335dcbd
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2026-09-12 12:39:23 +0200

    Completed attribution for bundled MXF tool components.

---
+++ docs/provenance/4.4-local-builds/mxf-tool-notices/asdcplib/src/KM_aes.cpp
@@ -0,0 +1,10 @@
+/*
+public domain code distributed with asdcplib
+
+copied from https://github.com/kokke/tiny-AES-c at 2020-07-15T23:38:12+00:00
+
+ORIGINAL NOTICE:
+"All material in this repository is in the public domain."
+
+Modified aggressively to remove unused features.
+*/
